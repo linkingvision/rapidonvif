821fb3a9ef4f355f0a01230f00d3a11da403e9a7
xsmart <xsamrt@163.com>
2014-05-17 23:34:48 +0800

    Add onvif GetReceivers

--- include/onvifclientreceiver.hpp
+++ include/onvifclientreceiver.hpp
@@ -21,6 +21,7 @@ public:
     ~OnvifClientReceiver();
 public:
 	/* Add function to here */
+	int GetReceivers(_trv__GetReceiversResponse & receivers);
 				
 private:
 	OnvifClientDevice &m_Device;
@@ -36,6 +37,29 @@ inline OnvifClientReceiver::OnvifClientReceiver(OnvifClientDevice &device)
 inline OnvifClientReceiver::~OnvifClientReceiver()
 {
 
+}
+
+int OnvifClientReceiver::GetReceivers(_trv__GetReceiversResponse & receivers)
+{
+	_trv__GetReceivers req;
+	string strUrl;
+	string strUser;
+	string strPass;
+	
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	
+	ReceiverBindingProxy  receiverProxy(SOAP_C_UTFSTRING);
+	receiverProxy.soap_endpoint =  strUrl.c_str();
+	
+	soap_wsse_add_Security(&receiverProxy);
+	soap_wsse_add_UsernameTokenDigest(&receiverProxy, "Id", 
+		strUser.c_str() , strPass.c_str());
+		
+	return receiverProxy.GetReceivers( &req, &receivers) ;
 }
 
 
