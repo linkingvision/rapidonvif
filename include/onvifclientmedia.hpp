74db36aa542ca06dbb17f1a0daaa5ce7f0632e01
xsmart <xsmart@163.com>
2014-07-16 22:14:43 +0800

    add John Blowers contributions

--- include/onvifclientmedia.hpp
+++ include/onvifclientmedia.hpp
@@ -20,26 +20,306 @@ public:
     OnvifClientMedia(OnvifClientDevice &device);
     ~OnvifClientMedia();
 public:
-    int GetProfiles(_trt__GetProfilesResponse &profiles);
-				
+	//Profile Functions
+    int OnvifClientMedia::GetProfiles(_trt__GetProfilesResponse &profiles);
+	int OnvifClientMedia::GetProfile(_trt__GetProfileResponse &profileResponse,string profileToken);
+	int OnvifClientMedia::CreateProfile(_trt__CreateProfileResponse &CreateProfileResponse,string Name,string token);
+	//Uri and streaming functions
+	int OnvifClientMedia::GetSnapshotUri(_trt__GetSnapshotUriResponse &SnapshotUriResponse,string profileToken);
+	int OnvifClientMedia::GetStreamUri(_trt__GetStreamUriResponse &StreamUriResponse,tt__StreamSetup &StreamSetup, string profileToken);
+	int OnvifClientMedia::GetServiceCapabilities(_trt__GetServiceCapabilitiesResponse &GetServiceCapResponse);
+	int OnvifClientMedia::StartMulticastStreaming(_trt__StartMulticastStreamingResponse &StartMulticastResponse,string profileToken);
+	int OnvifClientMedia::StopMulticastStreaming(_trt__StopMulticastStreamingResponse &StopMulticastResponse,string profileToken);
+	//Video Source Functions
+	int OnvifClientMedia::GetVideoSources(_trt__GetVideoSourcesResponse &GetVideoSourcesResponse);
+	int OnvifClientMedia::GetVideoSourceConfigurations(_trt__GetVideoSourceConfigurationsResponse &GetVideoSourceConfigResponse);
+	int OnvifClientMedia::SetVideoSourceConfiguration(_trt__SetVideoSourceConfigurationResponse &SetVideoSourceConfigResponse);
+	int OnvifClientMedia::GetVideoSourceConfiguration(_trt__GetVideoSourceConfigurationResponse &GetVideoSourceConfigResponse,string ConfigurationToken);
+	int OnvifClientMedia::GetVideoSourceConfigurationOptions(_trt__GetVideoSourceConfigurationOptionsResponse &GetVideoSourceConfigOptionsResponse, string ConfigurationToken,string profileToken);
+	//Meta data functions
+	int OnvifClientMedia::GetMetadataConfigurations(_trt__GetMetadataConfigurationsResponse &GetMetadataConfigurationsResponse);
+	int OnvifClientMedia::GetMetadataConfiguration(_trt__GetMetadataConfigurationResponse &GetMetadataConfigurationResponse,string profileToken);
+	int OnvifClientMedia::GetMetadataConfigurationOptions(_trt__GetMetadataConfigurationOptionsResponse &GetMetadataConfigurationOptionsResponse,string ConfigToken,string profileToken);
+
 private:
 	OnvifClientDevice &m_Device;
 	MediaBindingProxy  mediaProxy;
 
 };
 
-inline OnvifClientMedia::OnvifClientMedia(OnvifClientDevice &device)
+int OnvifClientMedia::GetMetadataConfigurationOptions(_trt__GetMetadataConfigurationOptionsResponse &GetMetadataConfigurationOptionsResponse,string ConfigToken,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__GetMetadataConfigurationOptions GetMetadataConfigurationOptionsReq;
+	GetMetadataConfigurationOptionsReq.ConfigurationToken = &ConfigToken;
+	GetMetadataConfigurationOptionsReq.ProfileToken = &profileToken;
+	return mediaProxy.GetMetadataConfigurationOptions(&GetMetadataConfigurationOptionsReq,&GetMetadataConfigurationOptionsResponse);
+}
+
+int OnvifClientMedia::GetMetadataConfiguration(_trt__GetMetadataConfigurationResponse &GetMetadataConfigurationResponse,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__GetMetadataConfiguration GetMetadataConfigurationReq;
+	GetMetadataConfigurationReq.ConfigurationToken = profileToken;
+	return mediaProxy.GetMetadataConfiguration(&GetMetadataConfigurationReq,&GetMetadataConfigurationResponse);
+}
+
+int OnvifClientMedia::GetMetadataConfigurations(_trt__GetMetadataConfigurationsResponse &GetMetadataConfigurationsResponse)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__GetMetadataConfigurations GetMetadataConfigurationsReq;
+
+	return mediaProxy.GetMetadataConfigurations(&GetMetadataConfigurationsReq,&GetMetadataConfigurationsResponse);
+}
+
+int OnvifClientMedia::GetVideoSourceConfigurationOptions(_trt__GetVideoSourceConfigurationOptionsResponse &GetVideoSourceConfigOptionsResponse, string ConfigurationToken,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__GetVideoSourceConfigurationOptions GetVideoSourceConfigOptionsReq;
+	GetVideoSourceConfigOptionsReq.ConfigurationToken = &ConfigurationToken;
+	GetVideoSourceConfigOptionsReq.ProfileToken = &profileToken;
+
+	return mediaProxy.GetVideoSourceConfigurationOptions(&GetVideoSourceConfigOptionsReq,&GetVideoSourceConfigOptionsResponse);
+}
+
+int OnvifClientMedia::SetVideoSourceConfiguration(_trt__SetVideoSourceConfigurationResponse &SetVideoSourceConfigResponse)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__SetVideoSourceConfiguration SetVideoSourceConfigReq;
+
+	return mediaProxy.SetVideoSourceConfiguration(&SetVideoSourceConfigReq,&SetVideoSourceConfigResponse);
+}
+
+int OnvifClientMedia::GetVideoSourceConfiguration(_trt__GetVideoSourceConfigurationResponse &GetVideoSourceConfigResponse,string ConfigurationToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__GetVideoSourceConfiguration GetVideoSourceConfigReq;
+	GetVideoSourceConfigReq.ConfigurationToken = ConfigurationToken;
+	return mediaProxy.GetVideoSourceConfiguration(&GetVideoSourceConfigReq,&GetVideoSourceConfigResponse);
+}
+
+int OnvifClientMedia::GetVideoSourceConfigurations(_trt__GetVideoSourceConfigurationsResponse &GetVideoSourceConfigResponse)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__GetVideoSourceConfigurations GetVideoSourceConfigReq;
+
+	return mediaProxy.GetVideoSourceConfigurations(&GetVideoSourceConfigReq,&GetVideoSourceConfigResponse);
+}
+
+int OnvifClientMedia::GetVideoSources(_trt__GetVideoSourcesResponse &GetVideoSourcesResponse)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__GetVideoSources GetVideoSourcesReq;
+
+	return mediaProxy.GetVideoSources(&GetVideoSourcesReq,&GetVideoSourcesResponse);
+}
+
+int OnvifClientMedia::CreateProfile(_trt__CreateProfileResponse &CreateProfileResponse,string Name,string token)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__CreateProfile CreateProfileReq;
+	CreateProfileReq.Name = Name;
+	CreateProfileReq.Token = &token;
+
+	return mediaProxy.CreateProfile(&CreateProfileReq,&CreateProfileResponse);
+}
+
+int OnvifClientMedia::StopMulticastStreaming(_trt__StopMulticastStreamingResponse &StopMulticastResponse,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__StopMulticastStreaming StopMulticastReq;
+	StopMulticastReq.ProfileToken = profileToken;
+
+	return mediaProxy.StopMulticastStreaming(&StopMulticastReq,&StopMulticastResponse);
+
+}
+
+int OnvifClientMedia::StartMulticastStreaming(_trt__StartMulticastStreamingResponse &StartMulticastResponse,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__StartMulticastStreaming StartMulticastReq;
+	StartMulticastReq.ProfileToken = profileToken;
+	return mediaProxy.StartMulticastStreaming(&StartMulticastReq,&StartMulticastResponse);
+
+}
+
+int OnvifClientMedia::GetServiceCapabilities(_trt__GetServiceCapabilitiesResponse &GetServiceCapResponse)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_trt__GetServiceCapabilities GetServiceCapReq;
+
+	return mediaProxy.GetServiceCapabilities(&GetServiceCapReq,&GetServiceCapResponse);
+
+}
+
+int OnvifClientMedia::GetStreamUri(_trt__GetStreamUriResponse &StreamUriResponse,tt__StreamSetup &StreamSetup, string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+
+	_trt__GetStreamUri StreamUriReq;
+	StreamUriReq.ProfileToken = profileToken;
+	StreamUriReq.StreamSetup = &StreamSetup;
+
+	return mediaProxy.GetStreamUri(&StreamUriReq, &StreamUriResponse);
+
+}
+
+OnvifClientMedia::OnvifClientMedia(OnvifClientDevice &device)
 : m_Device(device), mediaProxy(SOAP_C_UTFSTRING)
 {
 
 }
 
-inline OnvifClientMedia::~OnvifClientMedia()
+OnvifClientMedia::~OnvifClientMedia()
 {
 
 }
 
-inline int OnvifClientMedia::GetProfiles(_trt__GetProfilesResponse &profiles)
+int OnvifClientMedia::GetProfiles(_trt__GetProfilesResponse &profilesResponse)
 {
 	string strUrl;
 	string strUser;
@@ -58,8 +338,50 @@ inline int OnvifClientMedia::GetProfiles(_trt__GetProfilesResponse &profiles)
 	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", 
 		strUser.c_str() , strPass.c_str());
 		
-	return mediaProxy.GetProfiles( &profilesReq, &profiles) ;
+	return mediaProxy.GetProfiles( &profilesReq, &profilesResponse) ;
 }
+
+inline int OnvifClientMedia::GetProfile(_trt__GetProfileResponse &profileResponse,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false || m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", 
+		strUser.c_str() , strPass.c_str());
+		
+	_trt__GetProfile profileReq;
+	profileReq.ProfileToken = profileToken;
+	return mediaProxy.GetProfile( &profileReq, &profileResponse) ;
+}
+
+inline int OnvifClientMedia::GetSnapshotUri(_trt__GetSnapshotUriResponse &SnapshotUriResponse,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetMediaUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+
+	mediaProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&mediaProxy);
+	soap_wsse_add_UsernameTokenDigest(&mediaProxy, "Id", strUser.c_str() , strPass.c_str());
+
+
+	_trt__GetSnapshotUri SnapshotUriReq;
+
+	return mediaProxy.GetSnapshotUri(&SnapshotUriReq, &SnapshotUriResponse);
+
+}
+
 
 
 #endif /* __ONVIF_CLIENT_MEDIA__ */
--- include/onvifclientptz.hpp
+++ include/onvifclientptz.hpp
@@ -10,6 +10,7 @@
 #include "soapStub.h"
 #include "soapPTZBindingProxy.h"
 #include "wsseapi.h"
+//#include "soapC.h"
 
 
 using namespace std;
@@ -19,21 +20,365 @@ class OnvifClientPTZ
 public:
     OnvifClientPTZ(OnvifClientDevice &device);
     ~OnvifClientPTZ();
+
+	bool m_bHasRelativePTMove;
+	bool m_bHasRelativeZoomMove;
+	bool m_bHasAbsolutePTMove;
+	bool m_bHasAbsoluteZoomMove;
+	bool m_bHasContinuousPTMove;
+	bool m_bHasContinuousZoomMove;
+
 public:
 	/* Add function to here */
-				
+	//Configuration functions
+	int OnvifClientPTZ::GetConfiguration(_tptz__GetConfigurationResponse &configurationResp); // John 7/2
+	int OnvifClientPTZ::GetConfigurations(_tptz__GetConfigurationsResponse &configurationsResp);
+	int OnvifClientPTZ::GetConfigurationOptions(_tptz__GetConfigurationOptionsResponse &configOptions, string configToken); // John 7/2
+	int OnvifClientPTZ::SetConfiguration(_tptz__SetConfigurationResponse &SetConfiguration, tt__PTZConfiguration &PTZConfiguration,bool ForcePersist);
+	//Move Functions (includes Pan, Tilt, and Zoom Moves)
+	int OnvifClientPTZ::RelativeMove(_tptz__RelativeMoveResponse &relMoveResponse, tt__PTZVector &Translation, tt__PTZSpeed &Speed, string profileToken);
+	int OnvifClientPTZ::ContinuousMove(_tptz__ContinuousMoveResponse &ContMoveResponse, tt__PTZSpeed &Speed, LONG64 &Timeout, string profileToken);
+	int OnvifClientPTZ::AbsoluteMove(_tptz__AbsoluteMoveResponse &AbsoluteMoveResponse, tt__PTZSpeed &Speed, tt__PTZVector &position, string profileToken);
+	int OnvifClientPTZ::Stop(_tptz__StopResponse &StopResponse,bool PanTilt,bool Zoom,string profileToken);
+	//Zoom Functions
+	//TODO:
+
+	//Node, status, and service capabilities
+	int OnvifClientPTZ::GetNodes(_tptz__GetNodesResponse &GetNodesResponse);
+	int OnvifClientPTZ::GetNode(_tptz__GetNodeResponse &GetNodeResponse, string nodeToken);
+	int OnvifClientPTZ::GetStatus(_tptz__GetStatusResponse &StatusResponse,string profileToken);
+	int OnvifClientPTZ::GetServiceCapabilities(_tptz__GetServiceCapabilitiesResponse &ServiceCapResponse);
+
+	int OnvifClientPTZ::Initialize();
+
 private:
 	OnvifClientDevice &m_Device;
+	PTZBindingProxy ptzProxy;
 
 };
 
-inline OnvifClientPTZ::OnvifClientPTZ(OnvifClientDevice &device)
-: m_Device(device)
+
+/* Added by John 7/2/14 */
+int OnvifClientPTZ::Initialize()
 {
+	_tptz__GetConfigurationsResponse GetConfigsResp;
+	
+	if(GetConfigurations(GetConfigsResp)!=0)
+		return -1;
+
+	if(GetConfigsResp.PTZConfiguration[0]->DefaultAbsolutePantTiltPositionSpace) 
+		m_bHasAbsolutePTMove = TRUE; else m_bHasAbsolutePTMove = FALSE;
+	if(GetConfigsResp.PTZConfiguration[0]->DefaultAbsoluteZoomPositionSpace)
+		m_bHasAbsoluteZoomMove = TRUE; else m_bHasAbsoluteZoomMove = FALSE;
+	if(GetConfigsResp.PTZConfiguration[0]->DefaultRelativePanTiltTranslationSpace)
+		m_bHasRelativePTMove = TRUE; else m_bHasRelativePTMove = FALSE;
+	if(GetConfigsResp.PTZConfiguration[0]->DefaultRelativeZoomTranslationSpace)
+		m_bHasRelativeZoomMove = TRUE; else m_bHasRelativeZoomMove = FALSE;
+	if(GetConfigsResp.PTZConfiguration[0]->DefaultContinuousPanTiltVelocitySpace)
+		m_bHasContinuousPTMove = TRUE; else m_bHasContinuousPTMove = FALSE;
+	if(GetConfigsResp.PTZConfiguration[0]->DefaultContinuousZoomVelocitySpace)
+		m_bHasContinuousZoomMove = TRUE; else m_bHasContinuousZoomMove = FALSE;
+	
+	return 0;
 
 }
 
-inline OnvifClientPTZ::~OnvifClientPTZ()
+
+int OnvifClientPTZ::Stop(_tptz__StopResponse &StopResponse,bool PanTilt,bool Zoom,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_tptz__Stop StopReq;
+	StopReq.PanTilt = &PanTilt;
+	StopReq.ProfileToken = profileToken;
+	StopReq.Zoom = &Zoom;
+	return ptzProxy.Stop(&StopReq,&StopResponse);
+}
+
+	int OnvifClientPTZ::AbsoluteMove(_tptz__AbsoluteMoveResponse &AbsoluteMoveResponse, tt__PTZSpeed &Speed, tt__PTZVector &position, string profileToken)
+{
+	if(!m_bHasAbsolutePTMove || !m_bHasAbsoluteZoomMove) {
+		cout << "Error, device does not support Absolute movement" << endl;
+		//return -1;
+	}
+	string strUrl;
+	string strUser;
+	string strPass;
+
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_tptz__AbsoluteMove AbsoluteMoveReq;
+	AbsoluteMoveReq.ProfileToken = profileToken;
+	AbsoluteMoveReq.Position = &position;
+	AbsoluteMoveReq.Speed = &Speed;
+
+	return ptzProxy.AbsoluteMove(&AbsoluteMoveReq,&AbsoluteMoveResponse);
+
+}
+
+int OnvifClientPTZ::RelativeMove(_tptz__RelativeMoveResponse &relMoveResponse, tt__PTZVector &Translation, tt__PTZSpeed &Speed, string profileToken)
+{
+
+	if(!m_bHasRelativePTMove || !m_bHasRelativeZoomMove) {
+		cout << "Error, device does not support Relative movement" << endl;
+		//return -1;
+	}
+
+	string strUrl;
+	string strUser;
+	string strPass;
+
+	_tptz__RelativeMove relMoveReq;
+
+	relMoveReq.ProfileToken = profileToken;
+	relMoveReq.Translation = &Translation;
+	relMoveReq.Speed = &Speed;
+
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+
+	return ptzProxy.RelativeMove(&relMoveReq,&relMoveResponse);
+
+	//_tptz__GetNodes getNodesReq;
+	//_tptz__GetNodesResponse getNodesResp;
+
+	//ptzProxy.GetNodes(&getNodesReq,&getNodesResp);
+}
+
+int OnvifClientPTZ::ContinuousMove(_tptz__ContinuousMoveResponse &ContMoveResponse, tt__PTZSpeed &Speed, LONG64 &Timeout, string profileToken)
+{
+	if(!m_bHasContinuousPTMove || !m_bHasContinuousZoomMove) {
+		cout << "Error, device does not support Continuous movement" << endl;
+		//return -1;
+	}
+
+	string strUrl;
+	string strUser;
+	string strPass;
+
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_tptz__ContinuousMove ContMoveReq;
+	ContMoveReq.ProfileToken = profileToken;
+	ContMoveReq.Velocity = &Speed;
+	ContMoveReq.Timeout = &Timeout;
+
+	return ptzProxy.ContinuousMove(&ContMoveReq,&ContMoveResponse);
+}
+
+int OnvifClientPTZ::SetConfiguration(_tptz__SetConfigurationResponse &SetConfigurationResponse, tt__PTZConfiguration &PTZConfiguration,bool ForcePersist)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_tptz__SetConfiguration SetConfigurationReq;
+	SetConfigurationReq.PTZConfiguration = &PTZConfiguration;
+	SetConfigurationReq.ForcePersistence = ForcePersist;
+
+	return ptzProxy.SetConfiguration(&SetConfigurationReq,&SetConfigurationResponse);
+}
+
+int OnvifClientPTZ::GetServiceCapabilities(_tptz__GetServiceCapabilitiesResponse &ServiceCapResponse)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_tptz__GetServiceCapabilities ServiceCapReq;
+
+	return ptzProxy.GetServiceCapabilities(&ServiceCapReq,&ServiceCapResponse);
+
+}
+
+int OnvifClientPTZ::GetStatus(_tptz__GetStatusResponse &StatusResponse,string profileToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_tptz__GetStatus StatusReq;
+	StatusReq.ProfileToken = profileToken;
+	return ptzProxy.GetStatus(&StatusReq,&StatusResponse);
+
+}
+
+int OnvifClientPTZ::GetNode(_tptz__GetNodeResponse &GetNodeResponse, string nodeToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	_tptz__GetNode GetNodeReq;
+	GetNodeReq.NodeToken = nodeToken;
+
+	return ptzProxy.GetNode(&GetNodeReq,&GetNodeResponse);
+}
+
+int OnvifClientPTZ::GetNodes(_tptz__GetNodesResponse &GetNodesResponse)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+
+	_tptz__GetNodes GetNodesReq;
+
+	//ptzProxy.GetNodes(&GetNodesReq, &GetNodesResponse);
+
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", strUser.c_str() , strPass.c_str());
+
+	return ptzProxy.GetNodes(&GetNodesReq,&GetNodesResponse);
+
+}
+
+int OnvifClientPTZ::GetConfigurations(_tptz__GetConfigurationsResponse &configurationsResp)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	_tptz__GetConfigurations	configurationsReq;
+	
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", 
+		strUser.c_str() , strPass.c_str());
+		
+	return ptzProxy.GetConfigurations( &configurationsReq, &configurationsResp) ;
+}
+
+int OnvifClientPTZ::GetConfiguration(_tptz__GetConfigurationResponse &configurationResp)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	_tptz__GetConfiguration	configurationReq;
+	
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+	
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", 
+		strUser.c_str() , strPass.c_str());
+		
+	return ptzProxy.GetConfiguration( &configurationReq, &configurationResp) ;
+}
+
+int OnvifClientPTZ::GetConfigurationOptions(_tptz__GetConfigurationOptionsResponse &configOptions, string configToken)
+{
+	string strUrl;
+	string strUser;
+	string strPass;
+	_tptz__GetConfigurationOptions	configurationOptionsReq;
+	configurationOptionsReq.ConfigurationToken = configToken;
+	
+	if (m_Device.GetUserPasswd(strUser, strPass) == false 
+		|| m_Device.GetPTZUrl(strUrl) == false)
+	{
+		return SOAP_ERR;
+	}
+
+	ptzProxy.soap_endpoint =  strUrl.c_str();
+	
+	soap_wsse_add_Security(&ptzProxy);
+	soap_wsse_add_UsernameTokenDigest(&ptzProxy, "Id", 
+		strUser.c_str() , strPass.c_str());
+		
+	return ptzProxy.GetConfigurationOptions( &configurationOptionsReq, &configOptions) ;
+
+}
+
+/* END - Added by John 7/2/14 */
+
+OnvifClientPTZ::OnvifClientPTZ(OnvifClientDevice &device)
+: m_Device(device)
+{
+	if(m_Device.GetCapabilities()==SOAP_OK)
+		Initialize();
+}
+
+OnvifClientPTZ::~OnvifClientPTZ()
 {
 
 }
